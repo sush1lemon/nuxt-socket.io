d102c5ae46e66a7358b0458752e08a3b2d9741e2
Jezer Dave Bacquian <37240199+sush1lemon@users.noreply.github.com>
2023-04-13 17:11:35 +0800

    remove extends

--- src/server/presets/node-socket.ts
+++ src/server/presets/node-socket.ts
@@ -2,7 +2,7 @@ import { defineNitroPreset } from "nitropack";
 
 export default defineNitroPreset({
   entry: "#internal/nitro/entries/node-socket",
-  extends: "#internal/nitro/entries/node",
+  // extends: "internal/nitro-default/entries/node",
   serveStatic: true,
   commands: {
     preview: "node ./server/index.mjs",
