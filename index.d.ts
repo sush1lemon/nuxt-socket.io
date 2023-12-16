8acf88b05b4cbff2e3f05c7f0f1166d306af302a
sush1lemon <37240199+sush1lemon@users.noreply.github.com>
2023-12-16 19:54:30 +0800

    add type for internal/nitro

--- index.d.ts
+++ index.d.ts
@@ -13,3 +13,11 @@ declare module 'vue' {
         $io (): Socket
     }
 }
+
+declare module "#internal/nitro/utils" {
+  export * from "nitropack/dist/runtime/utils"
+}
+
+declare module "#internal/nitro/shutdown" {
+  export * from "nitropack/dist/runtime/shutdown"
+}
